93330a8909399454eff30e6cff29e40e25803bd6
Giovanne Bianchi <giovannebianchisilva@gmail.com>
2025-04-26 13:18:26 -0300

    010

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,11 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
+        return view('login');
+    }
+
+    public function loginSubmit(Request $request)
+    {
         echo $request->input('text_username');
         echo '<br />';
         echo $request->input('text_password');
