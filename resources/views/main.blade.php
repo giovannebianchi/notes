238744ecf8d7699d69b15a6e64fc39aec0e727a1
Giovanne Bianchi <giovannebianchisilva@gmail.com>
2025-04-26 11:58:12 -0300

    004

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,13 +4,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Main Controller</title>
+  <title>Main Controller - {{ $value }}</title>
 </head>
 
 <body>
   <h1>Welcome View and Blade!</h1>
   <hr>
-  <h3>The value is: <?= $value ?></h3>
+  <h3>The value is: {{ $value }}</h3>
 </body>
 
 </html>
