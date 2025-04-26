cbb2981a7a74b54e4bff99868c593dcb3a6058de
Giovanne Bianchi <giovannebianchisilva@gmail.com>
2025-04-26 11:44:44 -0300

    002

---
+++ resources/views/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Main Controller</title>
+</head>
+
+<body>
+  <h1>Welcome View and Blade!</h1>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,5 @@ Route::get('/', function () {
 Route::get('/about', function() {
     echo "About us";
 });
+
+Route::get('/main', [MainController::class, 'index']);
