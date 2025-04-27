6d2c14cb661ad51d20a1266991d3cd2640e99326
Giovanne Bianchi <giovannebianchisilva@gmail.com>
2025-04-27 12:40:31 -0300

    032

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
 use Illuminate\Http\Request;
@@ -26,7 +27,36 @@ class MainController extends Controller
 
     public function newNoteSubmit(Request $request)
     {
-      echo 'I\'m creating a new note';
+      // Validate request
+      $request->validate(
+        // Rules
+         [
+             'text_title' => 'required|min:3|max:200',
+             'text_note' => 'required|min:3|max:3000'
+         ],
+         // Error messages
+         [
+             'text_title.required' => 'O título é obrigatório',
+             'text_title.min' => 'O título deve ter no mínimo :min caracteres',
+             'text_title.max' => 'O título deve ter no mínimo :max caracteres',
+             'text_note.required' => 'A nota é obrigatória',
+             'text_note.min' => 'A nota deve ter no mínimo :min caracteres',
+             'text_note.max' => 'A nota deve ter no mínimo :max caracteres'
+         ]
+     );
+
+      // Get user id
+      $id = session('user.id');
+
+      // Create new note
+      $note = new Note();
+      $note->user_id = $id;
+      $note->title = $request->text_title;
+      $note->text = $request->text_note;
+      $note->save();
+
+      // Redirect to home
+      return redirect()->route('home');
     }
 
     public function editNote($id)
