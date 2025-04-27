6f23f89e3e3709a113982dcc1ef21b460ac691ae
Giovanne Bianchi <giovannebianchisilva@gmail.com>
2025-04-27 13:21:04 -0300

    034

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -70,6 +70,46 @@ class MainController extends Controller
       return view('edit_note', ['note' => $note]);
     }
 
+    public function editNoteSubmit(Request $request)
+    {
+      // Validate request
+      $request->validate(
+        // Rules
+         [
+             'text_title' => 'required|min:3|max:200',
+             'text_note' => 'required|min:3|max:3000'
+         ],
+         // Error messages
+         [
+             'text_title.required' => 'O título é obrigatório',
+             'text_title.min' => 'O título deve ter no mínimo :min caracteres',
+             'text_title.max' => 'O título deve ter no mínimo :max caracteres',
+             'text_note.required' => 'A nota é obrigatória',
+             'text_note.min' => 'A nota deve ter no mínimo :min caracteres',
+             'text_note.max' => 'A nota deve ter no mínimo :max caracteres'
+         ]
+     );
+
+     // Check if note id exists
+     if($request->note_id == null) {
+      return redirect()->route('home');
+     }
+
+      // Decrypt note_id
+     $id = Operations::decryptId($request->note_id);
+
+      // Load note
+      $note = Note::find($id);
+
+      // Update note
+      $note->title = $request->text_title;
+      $note->text = $request->text_note;
+      $note->save();
+
+      //Redirect to home
+      return redirect()->route('home');
+    }
+
     public function deleteNote($id)
     {
       // $id = $this->decryptId($id);
